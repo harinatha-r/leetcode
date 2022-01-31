251313d07a033330b865fbd49736feb7005ae144
Harinatha Radhakrishna <harinatha.radhakrishna@sap.com>
2022-01-31 10:54:21 +0530

    changes

---
+++ src/main/java/com/hari/leetcode/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,12 @@
+package com.hari.leetcode;
+
+public class RemoveDuplicatesfromSortedList {
+    static public ListNode deleteDuplicates(ListNode head) {
+        
+        return null;
+    }
+
+    public static void main() {
+        System.out.println("test");
+    }
+}
